b7d876fab41cf81ef450ae26aec50a597deacd17
hemanthskola <hemanthsridevikrishna@gmail.com>
2022-05-22 12:47:53 +0530

    cucumber2

--- src/main/java/CucumberRunner.java
+++ src/main/java/CucumberRunner.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CucumberRunner {
-}
+import java.net.URL;
+
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.DataProvider;
+//import org.testng.annotations.Parameters;
+//import org.testng.annotations.Test;
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+
+    @CucumberOptions(
+            //path of feature file
+            features = "src/test/java/Features",
+            //path of step definition file
+            glue = "C:/Users/user/eclipse-workspace/test2/src/test/resources/Glue.java"
+    )
+    public class CucumberRunner extends TestNGCucumberRunner {
+    }
+
+
---
+++ src/test/java/Features/1.feature
@@ -0,0 +1,9 @@
+@cucumberTest
+
+  Feature: Practicing the intelliJ merge
+    Description: Feature for practicing GIT and Pull Requests
+
+  Scenario: Test
+    Given User opens a website
+    When Google page is opened
+    Then Open Rahul Shetty Website and print Title of the page
